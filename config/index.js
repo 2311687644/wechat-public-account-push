afd288f72d88a9b87840fa8b761959132e67167a
2311687644 <114327994+2311687644@users.noreply.github.com>
2022-09-25 16:32:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -63,7 +63,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '怡宝',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oSwV_50J26qvm9VPtwQvwnr6ioqA',
       // 你想对他发送的模板消息的模板ID
@@ -82,17 +82,17 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '*生日', name: '老婆', year: '2003', date: '04-27',
         },
         {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '纪念日', year: '2021', date: '09-04',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '蔡佳明', year: '2002', date: '09-13',
         },
         {
-          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+          type: '节日', name: '在一起纪念日', year: '2021', date: '10-04',
         }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
